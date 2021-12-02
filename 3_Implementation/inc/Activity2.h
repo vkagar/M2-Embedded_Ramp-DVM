9c0f99fe7c74599a211983819ccb3e72e3b4f777
vkagar <94288413+vkagar@users.noreply.github.com>
2021-12-02 09:12:54 +0530

    Update Activity2.h

--- 3_Implementation/inc/Activity2.h
+++ 3_Implementation/inc/Activity2.h
@@ -1,1 +1,25 @@
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
 
+
+#include <util/delay.h>
+#include <avr/io.h>
+
+//Function Definitions
+
+//@brief Initialize the registers for ADC setup
+void InitADC();
+
+//@brief Gives the ADC values for the selected channel
+
+//@param[in] ch Channel number from 0-7
+//@return uint16_t ADC value
+
+uint16_t ReadADC(uint8_t ch);
+
+//@brief Return the ADC value to main function
+//@return uint16_t The ADC value
+uint16_t activity2_GetADC(void);
+
+
+#endif // ACTIVITY2_H_INCLUDED
