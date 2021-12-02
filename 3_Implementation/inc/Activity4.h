aa018ab41c7468fe01845105f46dcf8b4cb923d2
vkagar <94288413+vkagar@users.noreply.github.com>
2021-12-02 09:13:48 +0530

    Update Activity4.h

--- 3_Implementation/inc/Activity4.h
+++ 3_Implementation/inc/Activity4.h
@@ -1,1 +1,38 @@
+#ifndef ACTIVITY4_H_INCLUDED
+#define ACTIVITY4_H_INCLUDED
 
+#define F_CPU 16000000UL /**< Clock Frequency of MCU is 16 MHz */
+#define USART_BAUDRATE 9600 /**< Baud rate for serial communication  */
+#define UBRR_VALUE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) /**< Formula to calculate UBRR value */
+
+/**
+ * Include files
+ */
+#include <util/delay.h>
+#include <avr/io.h>
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Setting Baud rate and Enabling Rx and Tx
+ *
+ */
+void USARTInit();
+
+/**
+ * @brief Reading data
+ *
+ * @return int The read data
+ */
+int USARTRead();
+
+/**
+ * @brief Writing data to view in serial monitor
+ *
+ * @param[in] temp The ADC value
+ */
+void activity4_USARTWrite(uint16_t temp);
+
+#endif // ACTIVITY4_H_INCLUDED
